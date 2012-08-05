59e42bec2e2643dbeab08c9c70634b30c92e9be5
Jennie Evers-Corvina <sevvie.corvina@gmail.com>
2012-08-05 13:27:11 -0700

    Ombre Sugar -- The tastiest Perl-inspired sugar.
    I wrote a module to provide some syntactical sugar for creating lazy
    getter methods in Ruby classes. It is a mix-in that provides two
    methods: `attr_lazy_reader` and `attr_lazy.` It has yet to be
    extensively tested.

---
+++ lib/app/ombre/sugar.rb
@@ -0,0 +1,23 @@
+
+module App
+  module Ombre
+    module Sugar
+      def attr_lazy_reader *args
+        args.each { |e|
+          define_method(e) {
+            instance_variable_set("@#{e}", self.method("_build_#{e}").call) unless instance_variable_defined? "@#{e}"
+            instance_variable_get("@#{e}")
+          }
+        }
+      end
+
+      def attr_lazy *args
+        args.each { |arg|
+          attr_writer arg
+          attr_lazy_reader arg
+        }
+      end
+    end
+  end
+end
+        
